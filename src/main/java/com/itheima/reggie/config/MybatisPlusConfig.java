e00d7e2ce2ce9f2078084e09e1bba14481706616
unknown <2413752724@qq.com>
2023-09-05 08:48:12 +0800

    'cate'

---
+++ src/main/java/com/itheima/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.itheima.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 配置mybatis发的分页插件
+ */
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+}
--- src/main/java/com/itheima/reggie/entity/Employee.java
+++ src/main/java/com/itheima/reggie/entity/Employee.java
@@ -27,8 +27,10 @@ public class Employee implements Serializable {
 
     private Integer status;
 
+    @TableField(fill  = FieldFill.INSERT) // 插入时更新字段
     private LocalDateTime createTime;
 
+    @TableField(fill  = FieldFill.INSERT_UPDATE) // 更新时填充字段
     private LocalDateTime updateTime;
 
     @TableField(fill = FieldFill.INSERT)
